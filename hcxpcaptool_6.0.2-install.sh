818db00fce6129b7e346653324327c67b9f57c47
OutsiderLost <87493359+OutsiderLost@users.noreply.github.com>
2022-01-11 21:03:07 +0100

    Add files via upload

---
+++ hcxpcaptool_6.0.2-install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "=============================================="
+echo "Replacement of just missing -> hcxpacptool !!!"
+echo "=============================================="
+echo " "
+echo "1. Method -> update & expected installation"
+echo ""
+echo "wait..."
+
+sudo apt update && sleep 10 && sudo apt-get install libcurl4-openssl-dev libssl-dev pkg-config && sleep 10
+
+echo " "
+echo "Performed! :-)"
+echo " "
+echo "2. Method: download & unmount & make & move"
+echo " "
+echo "wait..."
+
+wget http://archive.ubuntu.com/ubuntu/pool/universe/h/hcxtools/hcxtools_6.0.2.orig.tar.gz && tar -xvf hcxtools_6.0.2.orig.tar.gz && rm hcxtools_6.0.2.orig.tar.gz
+
+cd hcxtools-6.0.2
+
+make
+
+ls && mv hcxpcaptool /usr/bin/
+
+echo " "
+echo "Succesfull Performed! :-)"
+echo " "
+echo "Unecesary files removed..."
+
+cd .. && rm -r hcxtools-6.0.2
+
+echo " "
+echo 'Testing "help" -> hcxpcaptool'
+
+hcxpcaptool -h
+
+echo " "
+echo "If all released good -> hcxpcaptool :-)"
