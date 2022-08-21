b5c16a6cb26aed2b486bb98e94c7aeffe58c832a
OutsiderLost <87493359+OutsiderLost@users.noreply.github.com>
2022-08-21 09:51:11 +0200

    Update hcxpcaptool_6.0.2-install.sh

--- hcxpcaptool_6.0.2-install.sh
+++ hcxpcaptool_6.0.2-install.sh
@@ -1,41 +1,73 @@
 #!/bin/bash
 
-echo "=============================================="
-echo "Replacement of just missing -> hcxpacptool !!!"
-echo "=============================================="
+#--------------------- hcxpcaptool replacement -----------------------
+echo -e "\n--------------------- hcxpcaptool replacement -----------------------\n"
+
+hcxpcapngtool --help >/dev/null 2>&1 || echo -e '\nhcxpcapngtool ERROR not found! -> EXIT !!! (apt install hcxtools)\n'
+hcxpcapngtool --help >/dev/null 2>&1 && echo -e '\nhcxpcapngtool SUCCESS found and working! continues process..\n' || exit
+
+# main dpdncs install for kali: apt update && apt install libcurl4-openssl-dev libssl-dev pkg-config -y
+
+#(short word modding: "blob" before "master"/repo.tar.gz or "main"/repo.tar.gz -> "raw")
+
+### RAW INSTALL ###
+repcap_raw () {
+errvar1='noset'
+wget https://github.com/OutsiderLost/hcxpcaptool-replacement/raw/main/hcxpcaptool-offline.tar.gz -O hcxpcaptool-offline.tar.gz || errvar1='err1'
+
+if [ "$errvar1" = "err1" ]; then
+[ -f hcxpcaptool-offline.tar.gz ] && rm hcxpcaptool-offline.tar.gz
+echo -e '\nNo found raw repo url -> EXIT !!!\n'
+## no exit -> use BUILD INSTALL !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+exit
+else # else
 echo " "
-echo "1. Method -> update & expected installation"
-echo ""
-echo "wait..."
+tar -xvzf hcxpcaptool-offline.tar.gz -C /usr/bin && rm hcxpcaptool-offline.tar.gz
+fi   # fi
 
-apt update && sleep 10 && yes | apt-get install libcurl4-openssl-dev libssl-dev pkg-config && sleep 10
+chmod +x /usr/bin/hcxpcaptool
+}
+[ -f /usr/bin/hcxpcaptool ] || repcap_raw
 
+### BUILD INSTALL ###
+repcap_build () {
+errvar2='noset'
+wget https://github.com/ZerBea/hcxtools/archive/refs/tags/6.0.2.tar.gz -O hcxtools_6.0.2.orig.tar.gz || wget http://old-releases.ubuntu.com/ubuntu/pool/universe/h/hcxtools/hcxtools_6.0.2.orig.tar.gz -O hcxtools_6.0.2.orig.tar.gz || wget http://ftp.debian.org/debian/pool/main/h/hcxtools/hcxtools_6.0.2.orig.tar.gz -O hcxtools_6.0.2.orig.tar.gz || errvar2='err2'
+
+if [ "$errvar2" = "err2" ]; then
+[ -f hcxtools_6.0.2.orig.tar.gz ] && rm hcxtools_6.0.2.orig.tar.gz
+echo -e '\nNo found archive url -> EXIT !!!\n'
+exit
+else # else
 echo " "
-echo "Performed! :-)"
+tar -xvzf hcxtools_6.0.2.orig.tar.gz && rm hcxtools_6.0.2.orig.tar.gz
+fi   # fi
+
+make -C hcxtools-6.0.2
 echo " "
-echo "2. Method: download & unmount & make & move"
-echo " "
-echo "wait..."
+[ -f hcxtools-6.0.2/hcxpcaptool ] || echo -e '\nmake ERROR! -> install dependencies (firs update)\n'
+[ -f hcxtools-6.0.2/hcxpcaptool ] || apt update
+[ -f hcxtools-6.0.2/hcxpcaptool ] || echo -e '\nInstall just main dependencies...\n'
+[ -f hcxtools-6.0.2/hcxpcaptool ] || apt install libcurl4-openssl-dev libssl-dev pkg-config -y || apt-get install libcurl4-openssl-dev libssl-dev pkg-config -y
+[ -f hcxtools-6.0.2/hcxpcaptool ] || make clean -C hcxtools-6.0.2 >/dev/null 2>&1
+[ -f hcxtools-6.0.2/hcxpcaptool ] || echo -e '\nmake probe again...\n'
+[ -f hcxtools-6.0.2/hcxpcaptool ] || make -C hcxtools-6.0.2
+[ -f hcxtools-6.0.2/hcxpcaptool ] || echo -e '\nmake ERROR again! -> install other dependencies...\n'
+[ -f hcxtools-6.0.2/hcxpcaptool ] || apt install libz-dev -y
+[ -f hcxtools-6.0.2/hcxpcaptool ] || echo " "
+[ -f hcxtools-6.0.2/hcxpcaptool ] || apt install libcurl4-gnutls-dev -y
+[ -f hcxtools-6.0.2/hcxpcaptool ] || make clean -C hcxtools-6.0.2 >/dev/null 2>&1
+[ -f hcxtools-6.0.2/hcxpcaptool ] || echo -e '\nmake probe the last...\n'
+[ -f hcxtools-6.0.2/hcxpcaptool ] || make -C hcxtools-6.0.2
+[ -f hcxtools-6.0.2/hcxpcaptool ] || echo -e '\nmake ERROR again! -> something error EXIT !!!\n'
+[ -f hcxtools-6.0.2/hcxpcaptool ] && echo -e '\nmake SUCCESS! -> operation test follows...\n' || exit
 
-wget http://archive.ubuntu.com/ubuntu/pool/universe/h/hcxtools/hcxtools_6.0.2.orig.tar.gz && tar -xvzf hcxtools_6.0.2.orig.tar.gz && rm hcxtools_6.0.2.orig.tar.gz
+mv hcxtools-6.0.2/hcxpcaptool /usr/bin
+rm -rf hcxtools-6.0.2
+chmod +x /usr/bin/hcxpcaptool
+}
 
-cd hcxtools-6.0.2
+hcxpcaptool --help >/dev/null 2>&1 || repcap_build
 
-make
-
-ls && mv hcxpcaptool /usr/bin/
-
-echo " "
-echo "Succesfull Performed! :-)"
-echo " "
-echo "Unecesary files removed..."
-
-cd .. && rm -r hcxtools-6.0.2
-
-echo " "
-echo 'Testing "help" -> hcxpcaptool -h'
-
-hcxpcaptool -h
-
-echo " "
-echo "If all released good -> hcxpcaptool !!! :-)"
+hcxpcaptool --help >/dev/null 2>&1 || echo -e '\nhcxpcaptool ERROR! not working -> EXIT !!!\n'
+hcxpcaptool --help >/dev/null 2>&1 && echo -e '\nhcxpcaptool SUCCESS working! continues process..\n' || exit
